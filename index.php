0f88f82a4352fb7aee293668bd529be8fe033dce
Alisson Custodio <forevergamer1@hotmail.com>
2024-07-12 12:38:23 -0300

    Projetin V.0 Front completo... fodase

--- index.php
+++ index.php
@@ -12,41 +12,39 @@
         <div class="header-menu">
             <ul class="header-menu-esq">
                 <li class="header-item">
-                    <a href="#"><i class='bx bxl-wordpress'></i></a>
+                    <a href="?pag=sobre"><i class='bx bxl-wordpress'></i></a>
                     <ul class="menu-interno">
-                        <li><a href="#">Sobre o WordPress</a></li>
+                        <li><a href="?pag=sobre">Sobre o WordPress</a></li>
                     </ul>
                 </li>
                 <li class="header-item">
-                    <a href="#"><i class='bx bx-home'></i>ProjetinWP</a>
+                    <a href="?pag=painel"><i class='bx bx-home'></i>ProjetinWP</a>
                     <ul class="menu-interno">
-                        <li><a href="#">Visitar site</a></li>
+                        <li><a href="?pag=painel">Visitar site</a></li>
                     </ul>
                 </li>
                 <li>
                     <a href="#"><i class='bx bx-message-square-dots'></i>0</a>
                 </li>
                 <li class="header-item">
-                    <a href="#"><i class='bx bx-message-square-add'></i>Novo</a>
+                    <a href="?pag=usuarios">
+                        <i class='bx bx-message-square-add'></i>Novo</a>
                     <ul class="menu-interno">
-                        <li><a href="#">Post</a></li>
-                        <li><a href="#">Mídia</a></li>
-                        <li><a href="#">Página</a></li>
-                        <li><a href="#">Usuário</a></li>
+                        <li><a href="?pag=usuarios">Usuário</a></li>
                     </ul>
                 </li>
             </ul>
         </div>
         <div class="header-menu-dir">
-                <a class="menu-dir" href="#">
+                <a class="menu-dir" href="?pag=configuracao">
                     <span>Olá, User</span>
                     <img src="assets/img/user-avatar.png" alt="user-avatar">
                     <div class="sub-menu-dir">
                         <div class="sub-menu-container">
                             <img src="assets/img/user-avatar.png" alt="user-avatar">
                             <div class="sub-menu-link">
-                                <a href="#">User<br>Editar perfil</a>
-                                <a href="#">Sair</a>
+                                <a href="?pag=configuracao">User<br>Editar perfil</a>
+                                <a href="logout.php">Sair</a>
                             </div>
                         </div>
                     </div>
@@ -57,55 +55,23 @@
         <ul id="recolher-menu" class="nav-menu ">
             <li id="menu-externo" class="nav-menu-item classRel">
                 <a onclick="mostrarMenu()" class="menu-item" href="?pag=painel">
-                    <i class='bx bx-message-square-add'></i>Painel</a>
+                <i class='bx bx-message-square-add'></i>Painel</a>
                 <ul id="menu-oculto" class="nav-menu-interno classAbs">
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                </ul>
-            </li>
-            <li id="menu-externo" class="nav-menu-item classRel">
-                <a onclick="mostrarMenu()" class="menu-item" href="?pag=post">
-                    <i class='bx bx-message-square-add'></i>Post</a>
-                <ul id="menu-oculto" class="nav-menu-interno classAbs">
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                </ul>
-            </li>
-            <li id="menu-externo" class="nav-menu-item classRel">
-                <a onclick="mostrarMenu()" class="menu-item" href="?pag=midia">
-                    <i class='bx bx-message-square-add'></i>Mídia</a>
-                <ul id="menu-oculto" class="nav-menu-interno classAbs">
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                </ul>
-            </li>
-            <li id="menu-externo" class="nav-menu-item classRel">
-                <a onclick="mostrarMenu()" class="menu-item" href="?pag=paginas">
-                    <i class='bx bx-message-square-add'></i>Páginas</a>
-                <ul id="menu-oculto" class="nav-menu-interno classAbs">
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                    <li><a href="#">aaaaaaaaaaa</a></li>
+                    <li><a href="?pag=painel">Início</a></li>
+                    <li><a href="?pag=sobre">Sobre o WordPress</a></li>
                 </ul>
             </li>
             <li id="menu-externo" class="nav-menu-item classRel">
                 <a onclick="mostrarMenu()" class="menu-item" href="?pag=usuarios">
                     <i class='bx bx-message-square-add'></i>Usuário</a>
                 <ul id="menu-oculto" class="nav-menu-interno classAbs">
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                    <li><a href="#">aaaaaaaaaaa</a></li>
+                    <li><a href="?pag=usuarios">Todos os Usuários</a></li>
+                    <li><a href="?pag=perfil">Perfil</a></li>
                 </ul>
             </li>
             <li id="menu-externo" class="nav-menu-item classRel">
                 <a onclick="mostrarMenu()" class="menu-item" href="?pag=configuracao">
                     <i class='bx bx-message-square-add'></i>Configurações</a>
-                <ul id="menu-oculto" class="nav-menu-interno classAbs">
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                    <li><a href="#">aaaaaaaaaaa</a></li>
-                </ul>
-            </li>
-            <li class="nav-menu-item">
-                <a onclick="recolherMenuLat()" class="menu-item" href="#">
-                    <i class='bx bx-message-square-add'></i>Recolher menu</a>
             </li>
         </ul>
     </nav>
@@ -116,14 +82,11 @@
                 case 'painel':
                     include_once("pages/painel.html");
                     break;
-                case 'post':
-                    include_once("pages/post.html");
+                case 'sobre':
+                    include_once("pages/sobre.html");
                     break;
-                case 'midia':
-                    include_once("pages/midia.html");
-                    break;
-                case 'paginas':
-                    include_once("pages/paginas.html");
+                case 'perfil':
+                    include_once("pages/perfil.html");
                     break;
                 case 'usuarios':
                     include_once("pages/usuarios.html");
--- js/scrips.js
+++ js/scrips.js
@@ -1,12 +1,1 @@
-function mostrarMenu(){
-    let removerRel = document.getElementById("menu-externo")
-    removerRel.classList.remove("classRel")
-    let removerAbs = document.getElementById("menu-oculto")
-    removerAbs.classList.remove("nav-menu-interno", "classAbs")
-    let add = document.getElementById("menu-oculto")
-    add.classList.add('interno-abaixo')
-}
-function recolherMenuLat(){
-    let btnRecolher = document.getElementById("recolher-menu")
-    btnRecolher.classList.toggle("active")
-}
+
